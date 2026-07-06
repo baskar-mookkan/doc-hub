7bb4d3f6181df9425533e09557d73ca6247da4d2
Baskar Mookkan <baskar.mookkan@cyncly.com>
2026-07-06 19:13:58 +0530

    Added github base url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,16 +15,16 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://baskar-mookkan.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/doc-hub/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'baskar-mookkan', // Usually your GitHub org/user name.
+  projectName: 'doc-hub', // Usually your repo name.
+  trailingSlash: false, // GitHub pages does not like trailing slashes
   onBrokenLinks: 'throw',
 
   // Even if you don't use internationalization, you can use this field to set
